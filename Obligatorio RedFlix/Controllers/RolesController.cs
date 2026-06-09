dae3d624f6512b4db61b3e1864c6fc09ad080ba2
fauvi <fauvi@FAUSTO>
2026-06-08 21:44:23 -0300

    Agregar archivos de proyecto.

---
+++ Obligatorio RedFlix/Controllers/RolesController.cs
@@ -0,0 +1,18 @@
+﻿using Obligatorio_RedFlix.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Obligatorio_RedFlix.Controllers
+{
+    public class RolesController : Controller
+    {
+        private RedFlixDBEntities db = new RedFlixDBEntities();
+
+        public ActionResult Index()
+        {
+            var roles = db.Roles.ToList();
+
+            return View(roles);
+        }
+    }
+}
---
+++ Obligatorio RedFlix/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Obligatorio_RedFlix.MvcApplication" Language="C#" %>
